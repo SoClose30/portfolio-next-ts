8bd8f44077617fa088d332b2c9e11801682da02f
Emma <emmanuelsalvocv@gmail.com>
2022-12-28 20:26:58 -0300

    agregada mayor variedad de íconos

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -15,6 +15,10 @@ const MainLayout = ({ children }: MainLayoutProps) => {
 					href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
 					rel="stylesheet"
 				/>
+				<link rel="shortcut icon" href="/images/favicon.ico" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 			</Head>
 			<NavBar />
 			<ResNavBar />
